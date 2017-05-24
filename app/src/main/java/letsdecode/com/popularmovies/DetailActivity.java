19ab5f471751aabd4b29f920e0b4cac6af1d3211
Jyoti Bajaj <jyoti.k.bajaj@gmail.com>
2017-05-23 18:37:24 -0700

    refactored detail activity

--- app/src/main/java/letsdecode/com/popularmovies/DetailActivity.java
+++ app/src/main/java/letsdecode/com/popularmovies/DetailActivity.java
@@ -1,5 +1,6 @@
 package letsdecode.com.popularmovies;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,29 +11,40 @@ import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
-    ImageView tv_PosterImageView;
+    public static final String MOVIE_DATA_KEY = "movieDataKey";
+
+    public static Intent createIntent(Activity activity, final MovieData movieData) {
+        Intent i = new Intent(activity, DetailActivity.class);
+        i.putExtra(DetailActivity.MOVIE_DATA_KEY, movieData);
+        return i;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        Intent i = getIntent();
-        String date = i.getStringExtra("date");
-        String title = i.getStringExtra("title");
-        double vote = i.getDoubleExtra("vote", 0);
-        String overview = i.getStringExtra("overview");
-        String poster_url = i.getStringExtra("poster_image");
-        tv_PosterImageView = (ImageView) findViewById(R.id.selected_poster_image);
+
+        //initializing the views
+        ImageView tv_PosterImageView = (ImageView) findViewById(R.id.selected_poster_image);
         TextView dateTextView = (TextView) findViewById(R.id.tv_release_date);
         TextView titleTextView = (TextView) findViewById(R.id.tv_title);
         TextView voteTextView = (TextView) findViewById(R.id.tv_vote);
         TextView overviewTextView = (TextView) findViewById(R.id.tv_overview);
-        dateTextView.setText(date);
-        titleTextView.setText(title);
-        String voteAverage = vote + "";
+
+
+        //receive data from intent
+        Intent i = getIntent();
+        MovieData movieData = i.getParcelableExtra(MOVIE_DATA_KEY);
+
+
+
+        //setting the data into views
+        dateTextView.setText(movieData.getRelease_date());
+        titleTextView.setText(movieData.getTitle());
+        String voteAverage = movieData.getVote_average() + "";
         voteTextView.setText(voteAverage);
-        overviewTextView.setText(overview);
-        Picasso.with(getApplicationContext()).load(poster_url).into(tv_PosterImageView);
+        overviewTextView.setText(movieData.getOverview());
+        Picasso.with(getApplicationContext()).load(movieData.getMoviePosterUrl()).into(tv_PosterImageView);
 
 
     }
